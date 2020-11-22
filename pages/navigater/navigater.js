9594f5ee3465e508127ff06226ca196fd220006e
TanZheling <tanzheling@foxmail.com>
2020-11-22 20:30:02 +0800

    服务器基本数据上传获取

--- pages/navigater/navigater.js
+++ pages/navigater/navigater.js
@@ -16,12 +16,14 @@ Page({
     console.log('fanhui');
   },
 
+  /*
   nextPage:function(e){
     wx.navigateTo({
       url: '../process/process?img='+this.data.bgPic,
     })
     },
-  
+  */
+
   chooseImage:function(e){
     var source = e.currentTarget.dataset;
     var srcType = source.way;
@@ -38,7 +40,7 @@ Page({
       }
     })
   },
-  /*  //////////与服务器对接
+    //////////与服务器对接
   nextPage: function (e) {
     var that = this;
     //将选择的图片作为全局数据
@@ -46,32 +48,82 @@ Page({
     wx.showToast({
       title: '正在处理', icon: 'loading', duration: 100000
     });
+
+    /*wx.uploadFile({
+      url: 'https://tanzheling.cn/', //服务器链接
+      filePath: that.data.bgPic,
+      method: 'POST',
+      name: 'file',
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+
+    })
+*/
+
+    var filePath = that.data.bgPic;
+    console.log(filePath),
+/*
+wx.getSystemInfo({
+	success: function(res){
+		if(res.platform == 'android'){
+      filePath = filePath.split('//')[1]  //安卓系统的解析方法
+      console.log(filePath)
+		}else{
+      filePath = filePath//.split('.')[2] + filePath.match(/\.[^.]+?$/)[0]
+      console.log(filePath)
+		}
+	}
+})
+*/
     wx.uploadFile({
-      url: '', //服务器链接
+      url: 'https://www.tanzheling.cn/upload', //服务器链接
       filePath: that.data.bgPic,
       name: 'file',
+     
       header: {
         'content-type': 'multipart/form-data'
       },
       success: function (res) {
         // console.log(res.data);
         wx.hideToast();
+        console.log(res.statusCode);
         if (res.statusCode == 200) {
-          var jj = JSON.parse(res.data);//成功的话，对结果的data，将json字符串转为json对象
+          console.log(res.data);
+          var json = res.data;
+          console.log(typeof json);
+          console.log(json==null);
+          if (typeof json != 'object') {
+            if (json != null) {
+              json = json.replace("\ufeff", "");
+              console.log(json);
+              json = JSON.stringify(json);
+              console.log(json);
+             // json = encodeURIComponent(json);
+             // console.log(json);
+              var jj = JSON.parse(json);
+              jj = JSON.parse(jj);
+              console.log(jj);
+              //json = decodeURIComponent(json);
+             // console.log(json);
+            }
+          }   
+        //  var jj = JSON.parse(json);//成功的话，对结果的data，将json字符串转为json对象
+          console.log(jj);
           console.log('200');
-          app.globalData.result = jj["roadname"]; //result存路名
-          app.globalData.accuracy = jj["accuracy"]; //accuracy存结果准确度
+          app.globalData.result = jj["prediction"]; //result存路名
+          app.globalData.accuracy = jj["probability"]; //accuracy存结果准确度
           console.log(app.globalData.result);    
           console.log(app.globalData.accuracy);              
           wx.navigateTo({
             url: '../process/process',
           })
         } else {
-          
-          wx.showModal({
-            title: '提示',
-            content: '服务器错误，请稍后重试！',
-          });
+         
+         // wx.showModal({
+           // title: '提示',
+           // content: '服务器错误，请稍后重试！',
+         // });
         }
       },
       fail: function (res) {
@@ -79,41 +131,12 @@ Page({
       }
     })
   },
-*/
+
   onLoad: function (options) {
-    if (app.globalData.userInfo) {
       this.setData({
         bgPic: options.img
         
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    
   }
 })
--- pages/process/process.js
+++ pages/process/process.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-  bgPic:null,
+  //bgPic:null,
   text1:null,
   text2:null,
   
@@ -16,7 +16,7 @@ Page({
         src: app.globalData.bgPic,
         text: [app.globalData.result,app.globalData.accuracy],
         success: res => {
-          this.bgPic = res.path
+          //this.bgPic = res.path
           console.log(res.width)
           console.log(res.height)
           console.log(app.globalData.result)
@@ -24,18 +24,11 @@ Page({
         }
       }),
         this.setData({
-          bgPic: options.img,
+          bgPic: app.globalData.bgPic,
           text1: app.globalData.result,
           text2: app.globalData.accuracy
 
         })
     },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+ 
 })
